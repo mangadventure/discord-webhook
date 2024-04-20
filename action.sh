1c6e2bc83c4bf1dd89cce6ab4e822b96bc6532ff
ObserverOfTime <chronobserver@disroot.org>
2024-04-20 15:08:45 +0300

    Use jq to create the json body

--- action.sh
+++ action.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o pipefail
+
 case "$JOB_STATUS" in
   success) STATUS_COLOR=$((0x448844)) ;;
   failure) STATUS_COLOR=$((0xD82828)) ;;
@@ -11,62 +13,67 @@ REPO_BRANCH="${GITHUB_BASE_REF:-$GITHUB_REF_NAME}"
 REPO_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
 REPO_TREE="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/tree/$REPO_BRANCH"
 
-COMMIT_SUBJECT="$(git -C "$GITHUB_WORKSPACE" \
-                  log -1 "$GITHUB_SHA" --pretty='%s')"
-COMMIT_MESSAGE="$(git -C "$GITHUB_WORKSPACE" \
-                  log -1 "$GITHUB_SHA" --pretty='%b')"
-COMMITTER_NAME="$(git -C "$GITHUB_WORKSPACE" \
-                  log -1 "$GITHUB_SHA" --pretty='%cN')"
-
-: "${COMMIT_MESSAGE//$'\r'/}"; : "${_//\"/\\\"}"; COMMIT_MESSAGE="${_//$'\n'/$'\\n'}"
+COMMIT_SUBJECT="$(git -C "$GITHUB_WORKSPACE" log -1 "$GITHUB_SHA" --pretty='%s')"
+COMMIT_MESSAGE="$(git -C "$GITHUB_WORKSPACE" log -1 "$GITHUB_SHA" --pretty='%b')"
+COMMITTER_NAME="$(git -C "$GITHUB_WORKSPACE" log -1 "$GITHUB_SHA" --pretty='%cN')"
 
 ACTION_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
 
 AVATAR="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
 
+export AVATAR ACTION_URL COMMIT_SUBJECT REPO_URL COMMIT_MESSAGE COMMITTER_NAME JOB_STATUS
+
 WEBHOOK_DATA=$'{
-  "username": "GitHub",
-  "avatar_url": "'$AVATAR'",
-  "embeds": [{
-    "color": '${STATUS_COLOR:-$((0xFFFFFF))}',
-    "author": {
-      "name": "'$GITHUB_REPOSITORY' #'$GITHUB_RUN_NUMBER$RUN_SUFFIX'",
-      "url": "'$ACTION_URL'",
-      "icon_url": "'$GITHUB_SERVER_URL/$GITHUB_ACTOR'.png?s=40"
+  username: "GitHub",
+  avatar: $ENV.AVATAR,
+  embeds: [{
+    color: $color|tonumber,
+    author: {
+      url: $ENV.ACTION_URL,
+      name: $author,
+      icon: $icon
     },
-    "title": "'$COMMIT_SUBJECT'",
-    "url": "'$REPO_URL'",
-    "description": "'$COMMIT_MESSAGE'",
-    "footer": {
-      "text": "'$COMMITTER_NAME'",
-      "icon_url": "'$GITHUB_SERVER_URL/$COMMITTER_NAME'.png?s=40"
+    title: $ENV.COMMIT_SUBJECT,
+    url: $ENV.REPO_URL,
+    description: $ENV.COMMIT_MESSAGE,
+    footer: {
+      text: $ENV.COMMITTER_NAME,
+      icon_url: $icon
     },
-    "fields": [{
-      "name": "Status",
-      "value": "'$JOB_STATUS'",
-      "inline": true
+    fields: [{
+      name: "Status",
+      value: $ENV.JOB_STATUS,
+      inline: true
     }, {
-      "name": "Branch",
-      "value": "[`'$REPO_BRANCH'`]('$REPO_TREE')",
-      "inline": true
+      name: "Branch",
+      value: $branch,
+      inline: true
     }, {
-      "name": "'${LANG_NAME:=Bash}'",
-      "value": "'${LANG_VERSION:=$BASH_VERSION}'",
-      "inline": true
+      name: $lang_name,
+      value: $lang_version,
+      inline: true
     }],
-    "timestamp": "'$(date --utc +%FT%TZ)'"
+    timestamp: $date
   }]
 }'
 
 SELF_URL='https://github.com/mangadventure/discord-webhook'
 
 printf '[INFO] Sending webhook to Discord...\n'
-if curl -Ssf "$WEBHOOK_URL" -d "$WEBHOOK_DATA" \
-    -A "Discord-Webhook ($SELF_URL, v0.1)" \
-    -H 'Content-Type: application/json'; then
+jq -Mn "$WEBHOOK_DATA" \
+  --arg color "${STATUS_COLOR:-$((0xFFFFFF))}" \
+  --arg author "$GITHUB_REPOSITORY #$GITHUB_RUN_NUMBER$RUN_SUFFIX" \
+  --arg icon "$GITHUB_SERVER_URL/$GITHUB_ACTOR.png?s=40" \
+  --arg branch "[$REPO_BRANCH]($REPO_TREE)" \
+  --arg lang_name "${LANG_NAME:-Bash}" \
+  --arg lang_version "${LANG_VERSION:-$BASH_VERSION}" \
+  --arg date "$(date --utc +%FT%TZ)" \
+  | curl "$WEBHOOK_URL" -H 'Content-Type: application/json' \
+     -Ssf -d@- -A "Discord-Webhook ($SELF_URL, v0.5)"
+ecode=$?
+if ((ecode == 0)); then
   printf '[INFO] Successfully sent the webhook.\n'
 else
-  ecode=$?
   printf '[ERROR] Failed to send the webhook.\n'
   exit $ecode
 fi
